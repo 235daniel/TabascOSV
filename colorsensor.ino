32fc0544261bc01a08c981b32027109cacc38d18
235daniel <44349659+235daniel@users.noreply.github.com>
2018-11-06 16:57:20 -0500

    Add files via upload

---
+++ colorsensor.ino
@@ -0,0 +1,85 @@
+#include <Wire.h>
+#include "Adafruit_TCS34725.h"
+
+Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
+
+//int redPin = 3;
+//int greenPin = 6;
+//int bluePin = 9;
+//byte gammatable[256];
+void setup() {
+  Serial.begin(9600);
+  if (tcs.begin()){
+    Serial.println("Found sensor");
+  } else {
+    Serial.println("Error: Did not find sensor");
+       while(1); 
+  }
+
+  //pinMode(redPin, OUTPUT);
+  //pinMode(greenPin, OUTPUT);
+  //pinMode(bluePin, OUTPUT);
+
+  /*for (int i=0; i<256; i++) {
+    float x = i;
+    x /= 255;
+    x = pow(x, 2.5);
+    x *= 255;
+    gammatable[i] = 255 - x; // Assuming RGB LED is anode, if cathode change to just x
+  } */
+}
+
+
+
+void loop() {
+  uint16_t clear, red, green, blue;
+  tcs.setInterrupt(false); // Turn on LED
+  delay(60);
+  tcs.getRawData(&red, &green, &blue, &clear);
+  
+  tcs.setInterrupt(true); // Turn off LED
+
+  Serial.print("C:\t"); Serial.print(clear);
+  Serial.print("\tR:\t"); Serial.print(red);
+  Serial.print("\tG:\t"); Serial.print(green);
+  Serial.print("\tB:\t"); Serial.print(blue);
+
+  uint32_t sum = clear;
+  float r, g, b;
+  
+  r = red; r /= sum;
+  g = green; g /= sum;
+  b = blue; b /= sum;
+  r *= 256; g *= 256; b *= 256;
+
+  Serial.print("\t");
+  Serial.print((int)r, HEX); Serial.print((int)g, HEX); Serial.print((int)b, HEX);
+
+  //Testing version of determining the material detected
+  Serial.print("\t");
+
+  String material = getMaterialType((int)r, (int)g, (int)b);
+  Serial.print("\t"); Serial.print((String) (int) r);
+  Serial.print("\t"); Serial.print((String) (int) g);
+  Serial.print("\t"); Serial.print((String)(int) b);
+  Serial.print("\t");
+  Serial.print(material);
+  Serial.println();
+
+  //analogWrite(redpin, gammatable[(int)r]);
+  //analogWrite(greenpin, gammatable[(int)g]);
+  //analogWrite(bluepin, gammatable[(int)b]);
+
+}
+String getMaterialType(int r, int g, int b){
+  String material = "Unknown";
+  if (r >= 91 && r <= 94 && g >= 90 && g <= 92 && b >= 65 && b <= 67){
+    material = "Air";
+  } else if (r >= 91 && r <= 113 && g >= 84 && g <= 92 && b >= 56 && b <= 72){
+    material = "Copper";
+  } else if (r >= 60 && r <= 86 && g >= 87 && g <= 101 && b >= 70 && b <= 88){
+    material = "Steel";
+  } 
+
+  return material;
+}
